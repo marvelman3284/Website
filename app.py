ea62afb3ee1da688e662b160e8f6b78d78f1fcf5
marvelman3284 <jedimaster3284@gmail.com>
2021-04-10 12:36:10 -0400

    add new routes

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__) 
 
 @app.route('/')
+def home():
+    return "hello world"
+
+@app.route('/portfolio')
 def index():
     return render_template('index.html')
