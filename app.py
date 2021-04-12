b34877aaea02a7b6a7cb52b6031aecf0e0a20da4
marvelman3284 <jedimaster3284@gmail.com>
2021-04-11 22:09:38 -0400

    Working typing effect

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) 
+app = Flask(__name__)
+
+# For writing out the AI
+# pen = '[ "nerdy", "pretty", "fun" ]'
 
 @app.route('/')
 def home():
-    return "hello world"
+    return render_template('home.html')
+
 
 @app.route('/portfolio')
 def index():
